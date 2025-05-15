61ff114ae040c0751dcb4f95fe1b7e99dbee2ba4
JaySlex <iles.belkessam@gmail.com>
2025-05-15 12:53:00 -0400

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBglllcowzeC2EBwqWhRmw_LIEwCR_S184",
@@ -14,4 +15,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+export { auth, db };
